666e822bfb59028b499fbcae74541fba125d5a50
Torjus Bakkene <torjus.bakkene@gmail.com>
2025-11-07 22:40:57 +0100

    interact with customer at front desk

--- customer.gd
+++ customer.gd
@@ -1,11 +1,11 @@
-extends StaticBody3D
+extends CharacterBody3D
 
 @onready var customer_spawn_location: Node3D = %CustomerSpawnLocation
 @onready var customer_purchase_location: Node3D = %CustomerPurchaseLocation
 @onready var customer_leave_location: Node3D = %CustomerLeaveLocation
 
 const SPEED = 3.0
-
+var is_finished = false
 var target_position: Vector3 = Vector3.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,3 +19,11 @@ func _process(delta: float) -> void:
 	if move_vector.length() > to_move_this_tick:
 		move_vector = move_vector.normalized() * to_move_this_tick
 	position += move_vector
+
+	if is_finished:
+		target_position = customer_leave_location.position
+	
+	if position.distance_to(customer_leave_location.position) <= 1.0:
+		is_finished = false
+		target_position = customer_purchase_location.position
+		position = customer_spawn_location.position
--- front_desk.gd
+++ front_desk.gd
@@ -7,6 +7,11 @@ extends Node3D
 var front_desk_original_material: Material = Material.new()  # placeholder
 var highlight_material: Material = Material.new()  # placeholder
 
+var player_is_at_desk: bool = false
+var no_customer = Node3D.new()
+var active_customer = Node3D.new()
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Front desk running")
@@ -14,13 +19,15 @@ func _ready() -> void:
 	highlight_material = front_desk_original_material.duplicate()
 	highlight_material.emission_enabled = true
 	highlight_material.emission = Color(1, 1, 0, 0.6)
-
+	no_customer.name = "NO_CUSTOMER"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var player_action = Input.get_action_strength("player1_action")
+	if player_action > 0.5 and player_is_at_desk:
+		if active_customer.name != "NO_CUSTOMER":
+			active_customer.is_finished = true
 
-var player_is_at_desk: bool = false
 
 func _on_player_service_area_body_entered(body: Node3D) -> void:
 	print("Body entered:", body)
@@ -36,3 +43,16 @@ func _on_player_service_area_body_exited(body: Node3D) -> void:
 		player_is_at_desk = false
 		print("Player is no longer at desk")
 		mesh_instance_3d.set_surface_override_material(0, front_desk_original_material)
+
+
+func _on_customer_area_body_entered(body: Node3D) -> void:
+	print("Customer area entered", body)
+
+	if body.name == "Customer":
+		print("Customer present")
+		active_customer = body
+
+func _on_customer_area_body_exited(body: Node3D) -> void:
+	if body.name == "Customer":
+		print("Customer left")
+		active_customer = no_customer
