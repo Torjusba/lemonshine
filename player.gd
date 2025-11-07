ff984a4998195d8e0436302725157dff9a1406f8
Torjus Bakkene <torjus.bakkene@gmail.com>
2025-11-07 21:08:41 +0100

    player turns in the movement direction

--- player.gd
+++ player.gd
@@ -1,22 +1,18 @@
 extends CharacterBody3D
 
+@onready var meshes: Node3D = $Meshes
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("player1_left", "player1_right", "player1_up", "player1_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -24,5 +20,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	if not direction.is_zero_approx():
+		meshes.basis = Basis.looking_at(direction) 
 	move_and_slide()
