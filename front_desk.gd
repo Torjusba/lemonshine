99e8c95b1a90e44ffd17c44c51764d16d046fb7d
Torjus Bakkene <torjus.bakkene@gmail.com>
2025-11-07 22:55:44 +0100

    fix bug where customer is not customer

--- front_desk.gd
+++ front_desk.gd
@@ -8,8 +8,7 @@ var front_desk_original_material: Material = Material.new()  # placeholder
 var highlight_material: Material = Material.new()  # placeholder
 
 var player_is_at_desk: bool = false
-var no_customer = Node3D.new()
-var active_customer = Node3D.new()
+var active_customer: Node3D = null
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,21 +17,19 @@ func _ready() -> void:
 	front_desk_original_material = mesh_instance_3d.get_active_material(0).duplicate()
 	highlight_material = front_desk_original_material.duplicate()
 	highlight_material.emission_enabled = true
-	highlight_material.emission = Color(1, 1, 0, 0.6)
-	no_customer.name = "NO_CUSTOMER"
+	highlight_material.emission = Color(1, 1, 0.4)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var player_action = Input.get_action_strength("player1_action")
 	if player_action > 0.5 and player_is_at_desk:
-		if active_customer.name == "Customer":
+		if active_customer != null:
+			if active_customer.name != "Customer":
+				print("BUG: active_customer is not Customer")
 			active_customer.is_finished = true
-		elif active_customer.name != "NO_CUSTOMER":
-			print("BUG: active customer is not customer:", active_customer)
 
 
 func _on_player_service_area_body_entered(body: Node3D) -> void:
-	print("Body entered:", body)
 	if body.name == "Player":
 		player_is_at_desk = true
 		print("Player is at desk")
@@ -48,8 +45,6 @@ func _on_player_service_area_body_exited(body: Node3D) -> void:
 
 
 func _on_customer_area_body_entered(body: Node3D) -> void:
-	print("Customer area entered", body)
-
 	if body.name == "Customer":
 		print("Customer present")
 		active_customer = body
@@ -57,4 +52,4 @@ func _on_customer_area_body_entered(body: Node3D) -> void:
 func _on_customer_area_body_exited(body: Node3D) -> void:
 	if body.name == "Customer":
 		print("Customer left")
-		active_customer = no_customer
+		active_customer = null
