c42609f495107362bf102257e2c96cfd45e90a28
Torjus Bakkene <torjus.bakkene@gmail.com>
2025-11-08 22:51:38 +0100

    coffee maker cleanup

--- workstations/coffee_maker.gd
+++ workstations/coffee_maker.gd
@@ -1,6 +1,5 @@
 extends Node3D
 
-@onready var front_desk: Node3D = $"."
 @onready var player_service_area: Area3D = $PlayerServiceArea
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 @onready var making_coffee_timer: Timer = $MakingCoffeeTimer
@@ -21,21 +20,19 @@ var coffee_cup: Item3D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Coffee maker running")
 	original_material = mesh_instance_3d.get_active_material(0).duplicate()
 	highlight_material = original_material.duplicate()
 	highlight_material.emission_enabled = true
 	highlight_material.emission = Color(1, 1, 0.4)
-	making_coffee_timer.start()  # TODO require an action with water and coffee beans
 
-func _looks_at_desk(body: Node3D) -> bool:
-	var pos_vec = (front_desk.global_position - body.global_position).normalized()
+func _looks_at_this(body: Node3D) -> bool:
+	var pos_vec = (self.global_position - body.global_position).normalized()
 	var direction = (-body.global_transform.basis.z) # Global forward direction
 	return pos_vec.dot(direction) > 0.5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var player_interacting = active_player and _looks_at_desk(active_player)
+	var player_interacting = active_player and _looks_at_this(active_player)
 	if player_interacting:
 		mesh_instance_3d.set_surface_override_material(0, highlight_material)
 	else:
@@ -63,8 +60,7 @@ func _process(_delta: float) -> void:
 
 	var can_make_coffee = not (needs_coffee_beans or needs_water)
 	if can_make_coffee and making_coffee_timer.is_stopped():
-		print("DEBUG: starting coffee timer")
-		making_coffee_timer.start()  # TODO require an action with water and coffee beans
+		making_coffee_timer.start()
 		brewing_particles.emitting = true
 
 func _on_player_service_area_body_entered(body: Node3D) -> void:
