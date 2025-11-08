c4f07b687e18d26e11876552cf6909ed98fa00e1
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2025-11-08 11:05:27 +0100

    player: apply transform to player node, not only meshes

--- player.gd
+++ player.gd
@@ -13,7 +13,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("player1_left", "player1_right", "player1_up", "player1_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := Vector3(input_dir.x, 0, input_dir.y).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -21,5 +21,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	if not direction.is_zero_approx():
-		meshes.basis = Basis.looking_at(direction) 
+		transform.basis = Basis.looking_at(direction)
 	move_and_slide()
